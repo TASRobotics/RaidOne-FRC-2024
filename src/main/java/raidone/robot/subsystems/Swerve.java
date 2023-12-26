f5e2d82cc8f11fbf5fd7029a3861edbbe56f0ba9
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-12-26 12:24:31 +0800

    Add Swerve constructor & member variables

--- src/main/java/raidone/robot/subsystems/Swerve.java
+++ src/main/java/raidone/robot/subsystems/Swerve.java
@@ -1,7 +1,52 @@
 package raidone.robot.subsystems;
 
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import raidone.robot.Constants.SwerveConstants;
 
 public class Swerve extends SubsystemBase {
+
+    private final WPI_PigeonIMU imu = new WPI_PigeonIMU(SwerveConstants.kImuID);
+
+    private final SwerveModule mLeftFrontModule, mRightFrontModule, mLeftRearModule, mRightRearModule;
+    private final SwerveDriveOdometry odometry;
     
+    public Swerve() {
+    
+        mLeftFrontModule = new SwerveModule(
+            SwerveConstants.kLeftFrontThrottleID, 
+            SwerveConstants.kLeftFrontRotorID, 
+            SwerveConstants.kLeftFrontCANCoderID, 
+            SwerveConstants.kLeftFrontRotorOffsetAngle
+        );
+
+        mRightFrontModule = new SwerveModule(
+            SwerveConstants.kRightFrontThrottleID, 
+            SwerveConstants.kRightFrontRotorID, 
+            SwerveConstants.kRightFrontCANCoderID, 
+            SwerveConstants.kRightFrontRotorOffsetAngle
+        );
+
+        mLeftRearModule = new SwerveModule(
+            SwerveConstants.kLeftRearThrottleID, 
+            SwerveConstants.kLeftRearRotorID, 
+            SwerveConstants.kLeftRearCANCoderID, 
+            SwerveConstants.kLeftRearRotorOffsetAngle
+        );
+
+        mRightRearModule = new SwerveModule(
+            SwerveConstants.kRightRearThrottleID, 
+            SwerveConstants.kRightRearRotorID, 
+            SwerveConstants.kRightRearCANCoderID, 
+            SwerveConstants.kRightRearRotorOffsetAngle
+        );
+
+        odometry = new SwerveDriveOdometry(SwerveConstants.kSwerveKinematics, mImu.getRotation2d(), getModulePositions());
+
+    }
+
+
+
 }
