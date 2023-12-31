0fb85e2f95c2ae421a6840194fdad7273e4f002d
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-12-31 15:01:17 +0800

    Add AutoChooser methods

--- src/main/java/raidone/robot/utils/AutoChooser.java
+++ src/main/java/raidone/robot/utils/AutoChooser.java
@@ -1,7 +1,38 @@
 package raidone.robot.utils;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
 public class AutoChooser {
 
-    
+    private SendableChooser<CommandBase> chooser;
+
+    // List of commands for autonomous
+    private CommandBase[] commands = {};
+
+    /**
+     * Creates tab in Shuffleboard with list of specified commands ({@link AutoChooser#commands})
+     */
+    public AutoChooser() {
+        chooser = new SendableChooser<>();
+        chooser.setDefaultOption("EmptyAuto", commands[0]);
+        for(CommandBase cmd : commands){
+            chooser.addOption(cmd.getName(), cmd);
+        }
+        Shuffleboard.getTab("Main")
+                .add("Auton Selection", chooser)
+                .withSize(3, 1)
+                .withPosition(2, 1);
+    }
+
+    /**
+     * Returns the currently selected autonomous command.
+     * 
+     * @return Currently selected autonomous command
+     */
+    public CommandBase getSelectedCommand() {
+        return chooser.getSelected();
+    }
 
 }
