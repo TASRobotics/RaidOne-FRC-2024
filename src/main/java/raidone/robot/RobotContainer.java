009bc00c1beaa380be8282ab3a997897367a1027
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-12-31 17:24:18 +0800

    Update methods & objects in RobotContainer.java

--- src/main/java/raidone/robot/RobotContainer.java
+++ src/main/java/raidone/robot/RobotContainer.java
@@ -4,17 +4,60 @@
 
 package raidone.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import raidone.robot.Constants.TeleopConstants;
+import raidone.robot.subsystems.Swerve;
 
 public class RobotContainer {
+
+	private static final Swerve swerve = new Swerve();
+	private final XboxController master = new XboxController(0);
+
+	/** The container for the robot. Contains subsystems, OI devices, and commands. */
 	public RobotContainer() {
     	configureBindings();
+
+		swerve.setDefaultCommand(
+			new RunCommand(() -> swerve.drive(
+				-MathUtil.applyDeadband(master.getLeftY(), TeleopConstants.kDriveDeadband),
+				-MathUtil.applyDeadband(master.getLeftX(), TeleopConstants.kDriveDeadband),
+				-MathUtil.applyDeadband(master.getRightX(), TeleopConstants.kDriveDeadband),
+				true),
+				swerve
+			)
+		);
 	}
 
+	/**
+	 * Use this method to define your trigger->command mappings. Triggers can be created via the
+	 * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+	 * predicate, or via the named factories in {@link
+	 * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+	 * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+	 * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+	 * joysticks}.
+	 */
 	private void configureBindings() {}
 
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
 	public Command getAutonomousCommand() {
-    	return Commands.print("No autonomous command configured");
+    	return Robot.getChooser().getSelectedCommand();
+	}
+
+	/**
+	 * Gets the swerve subsystem
+	 * 
+	 * @return Swerve object
+	 */
+	public static Swerve getSwerve() {
+		return swerve;
 	}
 }
