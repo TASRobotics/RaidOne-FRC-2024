cce70e9d773991388fa4f48bdaf0c7bcfb121035
TheoreticalPotato <121378855+TheoreticalPotato@users.noreply.github.com>
2024-01-19 17:01:00 +0800

    Refactor constants to remove SwerveModueConstants class

--- src/main/java/raidone/robot/Constants.java
+++ src/main/java/raidone/robot/Constants.java
@@ -86,12 +86,32 @@ public final class Constants {
         public static final NeutralModeValue THROTTLE_NEUTRAL_MODE = NeutralModeValue.Brake;
 
         // angle offset in rotations
-        public static final SwerveModule.SwerveModuleConstants[] MODULE_CONSTANTS = new SwerveModule.SwerveModuleConstants[] {
-            new SwerveModule.SwerveModuleConstants(1, 1, 2, 1, -0.114258),
-            new SwerveModule.SwerveModuleConstants(2, 3, 4, 2, -0.273193),
-            new SwerveModule.SwerveModuleConstants(3, 5, 6, 3, -0.096680),
-            new SwerveModule.SwerveModuleConstants(4, 7, 8, 4, -0.121582),
-        };
+        // public static final SwerveModule.SwerveModuleConstants[] MODULE_CONSTANTS = new SwerveModule.SwerveModuleConstants[] {
+        //     new SwerveModule.SwerveModuleConstants(1, 1, 2, 1, -0.114258),
+        //     new SwerveModule.SwerveModuleConstants(2, 3, 4, 2, -0.27193),
+        //     new SwerveModule.SwerveModuleConstants(3, 5, 6, 3, -0.096680),
+        //     new SwerveModule.SwerveModuleConstants(4, 7, 8, 4, -0.121582),
+        // };
+
+        public static final int THROTTLE_I_ID = 1;
+        public static final int ROTOR_I_ID = 2;
+        public static final int CAN_CODER_I_ID = 1;
+        public static final double MODULE_I_OFFSET = -0.114258;
+
+        public static final int THROTTLE_II_ID = 3;
+        public static final int ROTOR_II_ID = 4;
+        public static final int CAN_CODER_II_ID = 2;
+        public static final double MODULE_II_OFFSET = -0.27193;
+
+        public static final int THROTTLE_III_ID = 5;
+        public static final int ROTOR_III_ID = 6;
+        public static final int CAN_CODER_III_ID = 3;
+        public static final double MODULE_III_OFFSET = -0.096680;
+
+        public static final int THROTTLE_IV_ID = 7;
+        public static final int ROTOR_IV_ID = 8;
+        public static final int CAN_CODER_IV_ID = 4;
+        public static final double MODULE_IV_OFFSET = -0.121582;
     }
 
     public static final class AutoConstants { //TODO: The below constants are used in the example auto, and must be tuned to specific robot
--- src/main/java/raidone/robot/subsystems/Swerve.java
+++ src/main/java/raidone/robot/subsystems/Swerve.java
@@ -32,58 +32,79 @@ public class Swerve extends SubsystemBase {
         imu.setYaw(0);
 
         swerveModules = new SwerveModule[] {
-            new SwerveModule(MODULE_CONSTANTS[0]),
-            new SwerveModule(MODULE_CONSTANTS[1]),
-            new SwerveModule(MODULE_CONSTANTS[2]),
-            new SwerveModule(MODULE_CONSTANTS[3])
+                new SwerveModule(THROTTLE_I_ID, ROTOR_I_ID, CAN_CODER_I_ID, MODULE_I_OFFSET),
+                new SwerveModule(THROTTLE_II_ID, ROTOR_II_ID, CAN_CODER_II_ID, MODULE_II_OFFSET),
+                new SwerveModule(THROTTLE_III_ID, ROTOR_III_ID, CAN_CODER_III_ID, MODULE_III_OFFSET),
+                new SwerveModule(THROTTLE_IV_ID, ROTOR_IV_ID, CAN_CODER_IV_ID, MODULE_IV_OFFSET)
         };
 
-        swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.SWERVE_DRIVE_KINEMATICS, getGyroYaw(), getModulePositions());
+        swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.SWERVE_DRIVE_KINEMATICS, getGyroYaw(),
+                getModulePositions());
     }
 
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
-        SwerveModuleState[] swerveModuleStates =
-            Constants.Swerve.SWERVE_DRIVE_KINEMATICS.toSwerveModuleStates(
+        SwerveModuleState[] swerveModuleStates = Constants.Swerve.SWERVE_DRIVE_KINEMATICS.toSwerveModuleStates(
                 fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
-                                    translation.getX(), 
-                                    translation.getY(), 
-                                    rotation, 
-                                    getHeading()
-                                )
-                                : new ChassisSpeeds(
-                                    translation.getX(), 
-                                    translation.getY(), 
-                                    rotation)
-                                );
+                        translation.getX(),
+                        translation.getY(),
+                        rotation,
+                        getHeading())
+                        : new ChassisSpeeds(
+                                translation.getX(),
+                                translation.getY(),
+                                rotation));
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.MAX_SPEED);
 
-        for(SwerveModule mod : swerveModules){
-            mod.setDesiredState(swerveModuleStates[mod.getModuleConstants().MODULE_NUMBER -1], isOpenLoop);
-        }
-    }    
+        // for (SwerveModule mod : swerveModules) {
+        //     mod.setDesiredState(swerveModuleStates[mod.getModuleConstants().MODULE_NUMBER - 1], isOpenLoop);
+        // }
+
+        swerveModules[0].setDesiredState(swerveModuleStates[0], isOpenLoop);
+        swerveModules[1].setDesiredState(swerveModuleStates[1], isOpenLoop);
+        swerveModules[2].setDesiredState(swerveModuleStates[2], isOpenLoop);
+        swerveModules[3].setDesiredState(swerveModuleStates[3], isOpenLoop);
+        
+    }
 
     /* Used by SwerveControllerCommand in Auto */
     public void setModuleStates(SwerveModuleState[] desiredStates) {
         SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.MAX_SPEED);
         
-        for(SwerveModule mod : swerveModules){
-            mod.setDesiredState(desiredStates[mod.getModuleConstants().MODULE_NUMBER -1], false);
-        }
+        // for(SwerveModule mod : swerveModules){
+        //             mod.setDesiredState(desiredStates[mod.getModuleConstants().MODULE_NUMBER -1], false);
+        
+        swerveModules[0].setDesiredState(desiredStates[0], false);
+        swerveModules[1].setDesiredState(desiredStates[1], false);
+        swerveModules[2].setDesiredState(desiredStates[2], false);
+        swerveModules[3].setDesiredState(desiredStates[3], false);
+
     }
 
-    public SwerveModuleState[] getModuleStates(){
+    public SwerveModuleState[] getModuleStates() {
         SwerveModuleState[] states = new SwerveModuleState[4];
-        for(SwerveModule mod : swerveModules){
-            states[mod.getModuleConstants().MODULE_NUMBER -1] = mod.getState();
-        }
+        // for (SwerveModule mod : swerveModules) {
+        //     states[mod.getModuleConstants().MODULE_NUMBER - 1] = mod.getState();
+        // }
+
+        states[0] = swerveModules[0].getState();
+        states[1] = swerveModules[1].getState();
+        states[2] = swerveModules[2].getState();
+        states[3] = swerveModules[3].getState();
+
         return states;
     }
 
-    public SwerveModulePosition[] getModulePositions(){
+    public SwerveModulePosition[] getModulePositions() {
         SwerveModulePosition[] positions = new SwerveModulePosition[4];
-        for(SwerveModule mod : swerveModules){
-            positions[mod.getModuleConstants().MODULE_NUMBER -1] = mod.getPosition();
-        }
+        // for (SwerveModule mod : swerveModules) {
+        //     positions[mod.getModuleConstants().MODULE_NUMBER - 1] = mod.getPosition();
+        // }
+
+        positions[0] = swerveModules[0].getPosition();
+        positions[1] = swerveModules[1].getPosition();
+        positions[2] = swerveModules[2].getPosition();
+        positions[3] = swerveModules[3].getPosition();
+
         return positions;
     }
 
@@ -95,36 +116,41 @@ public class Swerve extends SubsystemBase {
         swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), pose);
     }
 
-    public Rotation2d getHeading(){
+    public Rotation2d getHeading() {
         return getPose().getRotation();
     }
 
-    public void setHeading(Rotation2d heading){
-        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), heading));
+    public void setHeading(Rotation2d heading) {
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(),
+                new Pose2d(getPose().getTranslation(), heading));
     }
 
-    public void zeroHeading(){
-        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), new Rotation2d()));
+    public void zeroHeading() {
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(),
+                new Pose2d(getPose().getTranslation(), new Rotation2d()));
     }
 
     public Rotation2d getGyroYaw() {
         return Rotation2d.fromDegrees(imu.getYaw().getValue());
     }
 
-    public void resetModulesToAbsolute(){
-        for(SwerveModule mod : swerveModules){
+    public void resetModulesToAbsolute() {
+        for (SwerveModule mod : swerveModules) {
             mod.resetToAbsolute();
         }
     }
 
     @Override
-    public void periodic(){
+    public void periodic() {
         swerveOdometry.update(getGyroYaw(), getModulePositions());
 
-        for(SwerveModule mod : swerveModules){
-            SmartDashboard.putNumber("Mod " + mod.getModuleConstants().MODULE_NUMBER + " CANcoder", mod.getCANcoder().getDegrees());
-            SmartDashboard.putNumber("Mod " + mod.getModuleConstants().MODULE_NUMBER + " Angle", mod.getPosition().angle.getDegrees());
-            SmartDashboard.putNumber("Mod " + mod.getModuleConstants().MODULE_NUMBER + " Velocity", mod.getState().speedMetersPerSecond);    
-        }
+        // for (SwerveModule mod : swerveModules) {
+        //     SmartDashboard.putNumber("Mod " + mod.getModuleConstants().MODULE_NUMBER + " CANcoder",
+        //             mod.getCANcoder().getDegrees());
+        //     SmartDashboard.putNumber("Mod " + mod.getModuleConstants().MODULE_NUMBER + " Angle",
+        //             mod.getPosition().angle.getDegrees());
+        //     SmartDashboard.putNumber("Mod " + mod.getModuleConstants().MODULE_NUMBER + " Velocity",
+        //             mod.getState().speedMetersPerSecond);
+        // }
     }
 }
