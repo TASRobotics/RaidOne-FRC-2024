36aaa0279da9922f4067d5b64c471791d0a97125
Theo <theojfagen@gmail.com>
2024-01-30 09:16:21 -0800

    set up test path

--- src/main/java/raidone/robot/subsystems/Swerve.java
+++ src/main/java/raidone/robot/subsystems/Swerve.java
@@ -55,23 +55,23 @@ public class Swerve extends SubsystemBase {
                 this::driveRelative, 
                 new HolonomicPathFollowerConfig( 
 
-                        new PIDConstants(20.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(0.0, 0.0, 0.0), // Translation PID constants
                         new PIDConstants(0.37, 0.0, 0.0), // Rotation PID constants
-                        2.5, // Max module speed, in m/s
-                        TRACK_WIDTH / 2, // Drive base radius in meters. Distance from robot center to furthest module.
+                        2.5, // Max module speed in m/s
+                        TRACK_WIDTH / 2, // Distance from robot center to furthest module in m
                         new ReplanningConfig() // Default path replanning config. See the API for the options here
                 ),
                 () -> {
                     // Boolean supplier that controls when the path will be mirrored for the red
                     // alliance
                     // This will flip the path being followed to the red side of the field.
-                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE  
 
                     var alliance = DriverStation.getAlliance();
                     if (alliance.isPresent()) {
-                        return alliance.get() != DriverStation.Alliance.Red;
+                        return alliance.get() == DriverStation.Alliance.Red;
                     }
-                    return true;
+                    return false;
                 },
                 this // Reference to this subsystem to set requirements
         );
