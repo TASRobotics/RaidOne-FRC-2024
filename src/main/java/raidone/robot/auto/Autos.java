fe9385bc193e6d51e01a30fd61c740d3a71b2b30
Theo <theojfagen@gmail.com>
2024-02-02 23:22:36 -0800

    Update to 2024.2.1

--- src/main/java/raidone/robot/auto/Autos.java
+++ src/main/java/raidone/robot/auto/Autos.java
@@ -6,7 +6,6 @@ import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import raidone.robot.commands.DrivePath;
