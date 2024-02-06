26981a64e27e0708445b20c99f13365dd546917c
0x5b62656e5d <dev.benjamink@gmail.com>
2024-02-06 23:03:49 +0800

    Implement auton selector in RobotContainer

--- src/main/java/raidone/robot/RobotContainer.java
+++ src/main/java/raidone/robot/RobotContainer.java
@@ -7,11 +7,11 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import raidone.robot.Auto.Autos;
 import raidone.robot.commands.*;
 import raidone.robot.subsystems.*;
 
@@ -25,6 +25,7 @@ import raidone.robot.subsystems.*;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+    
     /* Controllers */
     private final Joystick driver = new Joystick(0);
 
@@ -39,10 +40,10 @@ public class RobotContainer {
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
     private final JoystickButton zeroPose = new JoystickButton(driver, XboxController.Button.kX.value);
 
-    private SendableChooser<Command> autoChooser;
-
     /* Subsystems */
     private final Swerve swerve = new Swerve();
+
+    private Autos autos = new Autos(swerve);
 
 
     /**
@@ -59,6 +60,8 @@ public class RobotContainer {
 
         // Configure the button bindings
         configureButtonBindings();
+
+        SmartDashboard.putData(autos);
     }
 
     /**
@@ -81,10 +84,11 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return null;
+        return autos.get();
     }
     
     public Swerve getSwerve() {
         return swerve;
     }
+
 }
