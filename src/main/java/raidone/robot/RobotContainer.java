1c19ecf5d48b4124b41138ce6f2137f46f44324c
theo <theojfagen@gmail.com>
2024-02-19 09:14:12 -0800

    refactor to remove redundant code

--- src/main/java/raidone/robot/RobotContainer.java
+++ src/main/java/raidone/robot/RobotContainer.java
@@ -9,13 +9,14 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import monologue.Annotations.Log;
-import raidone.robot.Auto.Autos;
+// import raidone.robot.Auto.Autos;
 import raidone.robot.commands.*;
 import raidone.robot.subsystems.*;
 
@@ -29,7 +30,7 @@ import raidone.robot.subsystems.*;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    
+
     /* Controllers */
     private final Joystick driver = new Joystick(0);
 
@@ -37,23 +38,29 @@ public class RobotContainer {
     private final int translationAxis = XboxController.Axis.kLeftY.value;
     private final int strafeAxis = XboxController.Axis.kLeftX.value;
     private final int rotationAxis = XboxController.Axis.kRightX.value; // For controller
-    // private final int rotationAxis = Joystick.kDefaultTwistChannel; // For joystick
+    // private final int rotationAxis = Joystick.kDefaultTwistChannel; // For
+    // joystick
 
     /* Driver Buttons */
     private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
     private final JoystickButton zeroPose = new JoystickButton(driver, XboxController.Button.kX.value);
 
+    private final SendableChooser<Command> autoChooser;
+
     /* Subsystems */
-    private final Swerve swerve = new Swerve();
+    private final Swerve swerve = new Swerve();    
 
-    private Autos autos = new Autos(swerve);
-
+    // private Autos autos = new Autos(swerve);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
+
+        //* When creating commands for the autos:
+        // NamedCommands.registerCommand("exampleCommand", exampleSubsystem.exampleCommand());
+
         swerve.setDefaultCommand(
                 new TeleopSwerve(
                         () -> -driver.getRawAxis(translationAxis),
@@ -64,6 +71,10 @@ public class RobotContainer {
 
         // Configure the button bindings
         configureButtonBindings();
+
+        autoChooser = AutoBuilder.buildAutoChooser();
+        SmartDashboard.putData(autoChooser);
+
     }
 
     /**
@@ -77,7 +88,8 @@ public class RobotContainer {
     private void configureButtonBindings() {
         /* Driver Buttons */
         zeroGyro.onTrue(new InstantCommand(() -> swerve.zeroHeading()));
-        zeroPose.onTrue(new InstantCommand(() -> swerve.setPose(new Pose2d(new Translation2d(0,0), new Rotation2d(0)))));
+        zeroPose.onTrue(
+                new InstantCommand(() -> swerve.setPose(new Pose2d(new Translation2d(0, 0), new Rotation2d(0)))));
     }
 
     /**
@@ -86,22 +98,10 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        // return autos.get();
-        return AutoBuilder.buildAuto("testAuto");
+        return autoChooser.getSelected();
 
-        // SwerveModuleState[] states = new SwerveModuleState[4];
-        // states[0] = new SwerveModuleState(2.0, Rotation2d.fromDegrees(0));
-        // states[1] = new SwerveModuleState(2.0, Rotation2d.fromDegrees(0));
-        // states[2] = new SwerveModuleState(2.0, Rotation2d.fromDegrees(0));
-        // states[3] = new SwerveModuleState(2.0, Rotation2d.fromDegrees(0));
-
-        // return Commands.run( () ->
-        //     swerve.setModuleStates(states)
-        // );
-
-        
     }
-    
+
     public Swerve getSwerve() {
         return swerve;
     }
